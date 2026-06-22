ce43fcb9b64ca9ad1123493a47dafaf0a71466b4
SnakeFreak85 <saschad1711@gmail.com>
2026-06-22 16:09:02 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,28 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "terraristik-v1";
+
+self.addEventListener("install", event => {
   self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
